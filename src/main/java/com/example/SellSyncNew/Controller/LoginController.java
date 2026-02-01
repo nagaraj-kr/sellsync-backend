43398835a7964b7ff59fadd8407697c1c0e84a5a
nagaraj-kr <k.r.nagaraj02@gmail.com>
2026-02-01 14:50:25 +0530

    Remove CrossOrigin annotation from LoginController
    Removed CrossOrigin annotation for frontend port.

--- src/main/java/com/example/SellSyncNew/Controller/LoginController.java
+++ src/main/java/com/example/SellSyncNew/Controller/LoginController.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5500") // Allow your frontend port
+
 public class LoginController {
 
     @Autowired
