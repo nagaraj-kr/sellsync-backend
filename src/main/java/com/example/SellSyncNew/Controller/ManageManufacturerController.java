2fc656911ee7f172ed0478350d2c1b3b4c1db248
nagaraj-kr <k.r.nagaraj02@gmail.com>
2026-02-01 14:51:39 +0530

    Remove CrossOrigin annotation from ManageManufacturerController
    Removed CrossOrigin annotation for localhost.

--- src/main/java/com/example/SellSyncNew/Controller/ManageManufacturerController.java
+++ src/main/java/com/example/SellSyncNew/Controller/ManageManufacturerController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/manufacturer")
-@CrossOrigin(origins = "http://localhost:5500")
 public class ManageManufacturerController {
 
     @Autowired
