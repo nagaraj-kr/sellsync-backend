05287fb70e86733fcce43deeaec40e3ca6f3a4f5
nagaraj-kr <k.r.nagaraj02@gmail.com>
2026-02-09 12:02:18 +0530

    Update LoginController.java

--- src/main/java/com/example/SellSyncNew/Controller/LoginController.java
+++ src/main/java/com/example/SellSyncNew/Controller/LoginController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
