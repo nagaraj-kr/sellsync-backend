e22ec8598c68bb47eed62b2d9e8b7793af046ef0
nagaraj-kr <k.r.nagaraj02@gmail.com>
2026-03-13 18:56:28 +0530

    Update RegistrationService.java

--- src/main/java/com/example/SellSyncNew/Service/RegistrationService.java
+++ src/main/java/com/example/SellSyncNew/Service/RegistrationService.java
@@ -6,7 +6,6 @@ package com.example.SellSyncNew.Service;
 import com.example.SellSyncNew.Model.Admin;
 import com.example.SellSyncNew.Model.Manufacturer;
 import com.example.SellSyncNew.Model.Wholesaler;
-import com.example.SellSyncNew.Repository.AdminRepository;
 import com.example.SellSyncNew.Repository.ManufacturerRepository;
 import com.example.SellSyncNew.Repository.WholesalerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +15,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegistrationService {
 
-    @Autowired private AdminRepository adminRepo;
     @Autowired private ManufacturerRepository manufacturerRepo;
     @Autowired private WholesalerRepository wholesalerRepo;
     @Autowired private PasswordEncoder passwordEncoder;
 
-
-
-    public String registerAdmin(Admin admin) {
-        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
-        admin.setStatus("PENDING");
-        adminRepo.save(admin);
-        return "Admin registration request submitted!";
-    }
 
     public String registerManufacturer(Manufacturer manufacturer) {
         manufacturer.setPassword(passwordEncoder.encode(manufacturer.getPassword()));
