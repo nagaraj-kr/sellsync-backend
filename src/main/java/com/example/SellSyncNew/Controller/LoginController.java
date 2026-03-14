98c0e3068229474c86035a3bedd6c39ea667125a
nagaraj-kr <k.r.nagaraj02@gmail.com>
2026-03-14 14:45:47 +0530

    Update LoginController.java

--- src/main/java/com/example/SellSyncNew/Controller/LoginController.java
+++ src/main/java/com/example/SellSyncNew/Controller/LoginController.java
@@ -111,17 +111,23 @@ import com.example.SellSyncNew.Service.WholesalerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 import org.springframework.security.core.Authentication;
-import org.springframework.security.authentication.AuthenticationException;
+import org.springframework.security.core.AuthenticationException;
+
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-
-import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.Map;
